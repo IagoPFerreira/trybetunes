054c78a337b410175e6e9f5660ecca2904f43d0a
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 17:37:30 -0300

    Feat: Req1.1 - Cria o componente Login e o renderiza em Routes

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
+import Login from '../components/Login';
 
 class Routes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/search" component={Search} />
-          <Route path="/album/id" component={Album} />
-          <Route path="/favorites" component={Favorites} />
-          <Route path="/profile/edit" component={ProfileEdit} />
-          <Route path="/profile" component={Profile} />
-          <Route path="*" component={NotFound} />
+          <Route exact path="/" component={ Login } />
+          {/* <Route path="/search" component={ Search } /> */}
+          {/* <Route path="/album/id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="*" component={ NotFound } /> */}
         </Switch>
       </div>
     );
