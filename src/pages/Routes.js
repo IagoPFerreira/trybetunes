ff2f69eea64d478245f6e37b1de1935a8fc43e4e
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 17:45:35 -0300

    Feat: Req1.5 - Cria o componente Profile e o renderiza em Routes

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 import Album from '../components/Album';
 import Favorites from '../components/Favorites';
 import Login from '../components/Login';
+import Profile from '../components/Profile';
 import Search from '../components/Search';
 
 class Routes extends Component {
@@ -15,7 +16,7 @@ class Routes extends Component {
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           {/* <Route path="/profile/edit" component={ ProfileEdit } /> */}
-          {/* <Route path="/profile" component={ Profile } /> */}
+          <Route path="/profile" component={ Profile } />
           {/* <Route path="*" component={ NotFound } /> */}
         </Switch>
       </div>
