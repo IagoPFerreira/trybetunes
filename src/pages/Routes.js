ac616d7dd6e3bacdf0357da2b48fa2c817fa4d02
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 17:25:38 -0300

    Cria Rotas

---
+++ src/pages/Routes.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/search" component={Search} />
+          <Route path="/album/id" component={Album} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/profile/edit" component={ProfileEdit} />
+          <Route path="/profile" component={Profile} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Routes;
