461ffda98747a7a4913c09f01b48d30b54e0bb46
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 20:58:58 -0300

    Cria componente Button

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends Component {
+  render() {
+    const { disabled, onClick, testId, text } = this.props;
+    return (
+      <button
+        type="button"
+        disabled={ disabled }
+        data-testid={ testId }
+        onClick={ onClick }
+      >
+        { text }
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  testId: PropTypes.string,
+  text: PropTypes.string,
+}.isRequired;
+
+export default Button;
