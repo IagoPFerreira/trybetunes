14964ed3ceb61ccef5da78e56991432a7676cb7a
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 20:45:55 -0300

    Feat: Req5.2 - Implementa a lógica de eable/disable do botao no component Search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabled: true,
+    };
+  }
+
+  checkNameLength = ({ target: { value } }) => {
+    const minimunNameLength = 2;
+    if (value.length >= minimunNameLength) {
+      this.setState({ disabled: false });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input type="text" data-testid="search-artist-input" />
-        <button type="button" data-testid="search-artist-button">Pesquisar</button>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.checkNameLength }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
