04b0b5215532870533aa5ca9121e64649ba3614f
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 20:04:18 -0300

    Feat: Req3.6 - Chama a funcao getUser ao renderizar o Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  componentDidMount() {
+    getUser();
+  }
+
   render() {
     return (
       <div data-testid="header-component" />
