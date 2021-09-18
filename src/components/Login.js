3d73f2f2cb936f8c6ef90897849e2e97bb1be6d7
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 18:07:54 -0300

    Feat: Req2.2 - Implementa a lógica de eable/disable do botao

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabled: true,
+    };
+  }
+
+  checkNameLength = ({ target: { value } }) => {
+    const minimunNameLength = 3;
+    if (value.length >= minimunNameLength) {
+      this.setState({ disabled: false });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-login">
-        <input data-testid="login-name-input" />
+        <input
+          data-testid="login-name-input"
+          onChange={ this.checkNameLength }
+        />
         <button
           type="button"
-          disabled
+          disabled={ disabled }
           data-testid="login-submit-button"
         >
           Entrar
