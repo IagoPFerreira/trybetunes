a69f6db372f5928a62697287fb752a2b9d719b75
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 18:01:24 -0300

    Feat: Req2.1 - Cria input e botao no componente Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input data-testid="login-name-input" />
+        <button
+          type="button"
+          disabled
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
+      </div>
     );
   }
 }
