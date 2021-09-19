447f4050ace7b544ceed37457b7676ff1fc0b422
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-19 14:09:44 -0300

    Feat: Req10.1 - Implementa a chamada da funcao getFavoriteSongs

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  componentDidMount() {
+    getFavoriteSongs().then((result) => console.log(result));
+  }
+
   render() {
     return (
       <div data-testid="page-favorites">
