a4af11c7f32db8beb5b3df14e9f831501ef0495a
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-18 21:38:38 -0300

    Feat: Req6.1 - Implementa a lógica de Loading no componente Search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
 
     this.state = {
+      isLoading: false,
       disabled: true,
+      artistName: '',
     };
   }
 
   checkNameLength = ({ target: { value } }) => {
     const minimunNameLength = 2;
     if (value.length >= minimunNameLength) {
-      this.setState({ disabled: false });
+      const artistName = value.split(' ')
+        .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+        .join(' ');
+      this.setState({ disabled: false, artistName });
     }
   }
 
+  handleSearch = () => {
+    const { artistName } = this.state;
+    this.setState({ isLoading: true });
+    console.log(artistName);
+    searchAlbumsAPI(artistName).then(() => {
+      this.setState({
+        isLoading: false,
+      });
+    });
+  }
+
   render() {
-    const { disabled } = this.state;
+    const { isLoading, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -30,10 +48,11 @@ class Search extends Component {
         />
         <Button
           disabled={ disabled }
-          onClick={ console.log('clicou') }
+          onClick={ this.handleSearch }
           testId="search-artist-button"
           text="Pesquisar"
         />
+        { isLoading ? <Loading /> : <p>Opa</p> }
       </div>
     );
   }
