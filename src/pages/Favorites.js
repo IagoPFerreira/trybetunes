e2dbd2de09fb6730226fe4bc8415e72cae6c756b
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-19 14:20:18 -0300

    Feat: Req10.2 - Renderiza o componente MusicCard em Favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      musics: '',
+    };
+  }
+
   componentDidMount() {
-    getFavoriteSongs().then((result) => console.log(result));
+    getFavoriteSongs().then((musics) => {
+      this.setState({
+        isLoading: false,
+        musics,
+      });
+    });
+  }
+
+  renderMusics = () => {
+    const { musics } = this.state;
+
+    return musics
+      .map((music, index) => <MusicCard key={ index } index={ index } music={ music } />);
   }
 
   render() {
+    const { isLoading } = this.state;
+    console.log(isLoading);
     return (
       <div data-testid="page-favorites">
         <Header />
+        { isLoading ? <Loading /> : this.renderMusics() }
       </div>
     );
   }
