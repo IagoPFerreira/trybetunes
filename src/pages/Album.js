25f37a042a4de4074e1e7ab1548d62aa917e3cf6
Iago Ferreira <iago.pferreiravr@gmail.com>
2021-09-19 11:00:51 -0300

    Feat: Req7.1 - Implementa a requisicao de um album por id

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  renderMusics = () => {
+    const { match: { params: { id } } } = this.props;
+    getMusics(id).then((results) => {
+      console.log(results);
+    });
+    return (<>Oi</>);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
+        { this.renderMusics() }
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default Album;
