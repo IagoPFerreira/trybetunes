b449b91fea708335d16ccd2307f13e943cddbf6e
iago <iago.pferreiravr@gmail.com>
2021-09-24 09:47:02 -0300

    Feat: Req11.1 - Implementa a chamada da funcao getUser

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    getUser().then((result) => {
+      this.setState({
+        isLoading: false,
+        user: result,
+      });
+    });
+  }
+
   render() {
+    const { isLoading, user } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { isLoading ? <Loading /> : console.log(user) }
       </div>
     );
   }
