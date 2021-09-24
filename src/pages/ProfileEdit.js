166a85883668bc35cde91c98ddad953319d875fd
iago <iago.pferreiravr@gmail.com>
2021-09-24 10:03:07 -0300

    Feat: Req12.1 - Implementa a chamada da funcao getUser

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    getUser().then((result) => {
+      this.setState({
+        isLoading: false,
+        user: result,
+      });
+    });
+  }
+
   render() {
     return (
       <div data-testid="page-profile-edit">
